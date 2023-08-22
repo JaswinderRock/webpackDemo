961969d8b1c2815b680621bd55cb78d7c6c3a4bd
JaswinderRock <jaswinder@reckonsys.com>
2023-08-22 20:13:16 +0530

    add babel and webpack

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Component, ReactNode } from "react";
+
+export class App extends Component {
+    render() {
+        return (
+            <h1>React TypeSript Webpack Starter Template</h1>
+        )
+    }
+}
